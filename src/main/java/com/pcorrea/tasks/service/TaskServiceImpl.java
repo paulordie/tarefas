51c8d55d4661784e3403f0a12dee3e76e3ed7d79
Paulo Correa <paulo.correa@indt.org.br>
2019-08-24 19:35:07 -0400

    add save repository to task of task

--- src/main/java/com/pcorrea/tasks/service/TaskServiceImpl.java
+++ src/main/java/com/pcorrea/tasks/service/TaskServiceImpl.java
@@ -16,4 +16,9 @@ public class TaskServiceImpl implements TaskService{
     public Iterable<Task> list() {
         return this.taskRepository.findAll();
     }
+
+    @Override
+    public Task save(Task task) {
+        return this.taskRepository.save(task);
+    }
 }
