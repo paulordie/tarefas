5f661ec37c17d0cbd3a7237d84cc31654166aa0d
Paulo Correa <paulo.correa@indt.org.br>
2019-08-24 19:24:46 -0400

    add service in taskcontroller and taskserviceimpl

--- src/main/java/com/pcorrea/tasks/controller/TaskController.java
+++ src/main/java/com/pcorrea/tasks/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.pcorrea.tasks.controller;
 
 
 import com.pcorrea.tasks.domain.Task;
+import com.pcorrea.tasks.service.TaskService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/tasks")
 public class TaskController {
 
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping( value = {"","/"})
     public Iterable<Task> list(){
-        return null;
+        return this.taskService.list();
     }
 
 }
