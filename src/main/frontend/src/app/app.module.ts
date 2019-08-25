f729faf236291f5b556b0e773eb88ab4bf7cfd91
Paulo Correa <paulo.correa@indt.org.br>
2019-08-25 16:22:14 -0400

    add tasks.service.ts config and localhost api path

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TasksAddComponent } from './tasks/tasks-add/tasks-add.component';
 import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
+import { HttpClientModule } from "@angular/common/http";
+import {TaskService} from "./tasks/task.service";
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
     TasksListComponent
   ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        HttpClientModule // adicionado!!!
     ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,10 +1,17 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+//import "rxjs/add/operator/map";
+import {map} from "rxjs/operators";
+
+@Injectable()
 export class TaskService {
 
-    constructor(){
+    constructor(private http: HttpClient){
 
     }
 
     getTasks(){
+        return this.http.get('/api/tasks').pipe(map(reponse => reponse.json()));
 
     }
 }
