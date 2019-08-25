934aeb7a2264b8466c9e025ed8b007e56171cd73
Paulo Correa <paulo.correa@indt.org.br>
2019-08-25 12:59:34 -0400

    add manual service with task.model.ts and work with tasks-list.component.ts

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -13,6 +13,18 @@ export class TasksListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      this.tasks.push( new Task(1,"Task 1", true, "25/08/2019"));
+      this.tasks.push( new Task(2,"Task 2", false, "25/08/2019"));
+      this.tasks.push( new Task(3,"Task 3", false, "25/08/2019"));
+  }
+
+  getDueDateLabel(task: Task){
+      return task.completed ? 'label-success' : 'label-primary';
+  }
+
+  onTaskChange(event, task){
+      //this.taskService.saveTask(task,event.target.checked).subscribe();
+      console.log('Task has changed');
   }
 
 }
