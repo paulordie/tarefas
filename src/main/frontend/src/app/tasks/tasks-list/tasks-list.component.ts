0869f4b3ee37e194bd5aef3ee8592d90a0922f2d
Paulo Correa <paulo.correa@indt.org.br>
2019-08-25 12:20:40 -0400

    Add task.model.ts to hold the id to spring

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+
+    tasks: Task[]=[];
 
   constructor() { }
 
