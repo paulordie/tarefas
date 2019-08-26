ffd2b9ebe7bbb9daae5a9b48356ef4a8db336437
Paulo Correa <paulo.correa@indt.org.br>
2019-08-26 08:57:12 -0400

    fix map() but do not show json on frontend already

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,17 +1,22 @@
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpRequest} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 //import "rxjs/add/operator/map";
 import {map} from "rxjs/operators";
+import {Observable} from "rxjs";
+import {Task} from "./task.model";
+
 
 @Injectable()
 export class TaskService {
+
+    taskUrl = 'http://localhost:8080/api/tasks';
 
     constructor(private http: HttpClient){
 
     }
 
-    getTasks(){
-        return this.http.get('/api/tasks').pipe(map(reponse => reponse.json()));
-
+    getTasks(): Observable<Task[]>{
+        // return this.http.get('/api/tasks').map(response => response.json());
+        return this.http.get<Task[]>(this.taskUrl).pipe(map((response: any) => response.json()));
     }
 }
