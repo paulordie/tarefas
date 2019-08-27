e32109a601f00a65ef929d01bcbc6919ec4f9866
Paulo Correa <paulo.correa@indt.org.br>
2019-08-27 16:30:45 -0400

    add select and save function it is work and margin data correct

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -29,7 +29,7 @@ export class TasksListComponent implements OnInit {
   }
 
   getDueDateLabel(task: Task){
-      return task.completed ? 'label-success' : 'label-primary';
+      return task.completed ? ' label-success' : ' label-primary';
   }
 
   onTaskChange(event, task){
